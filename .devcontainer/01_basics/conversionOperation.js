7e048eb4e70dc8e572d9b33732db2c1e327f3112
princekhurana16 <77041387+princekhurana16@users.noreply.github.com>
2024-11-16 10:21:51 +0000

    ConversionOfDatatypes

---
+++ .devcontainer/01_basics/conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "prince"
+
+//console.log(typeof score);
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+
+// "33" => 33
+// "33abc" => NaN but type is number 
+// true => 1; false => 0
+
+
+let isLoggedIn = "prince"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1=> true; 0=> False
+// ""=> false
+// "Prince"=> True
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
