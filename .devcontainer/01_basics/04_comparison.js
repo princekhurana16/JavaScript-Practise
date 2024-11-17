4a80620d826a0e6b2d41cfede2775f8c958a7dcd
princekhurana16 <77041387+princekhurana16@users.noreply.github.com>
2024-11-17 14:28:45 +0000

    comparision of data types

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+//console.log(2 > 1);
+//console.log(2 >= 1);
+//console.log(2 <= 1);
+//console.log(2 == 1);
+//console.log(2 != 1);
+
+//console.log("2" > 1);
+//console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// Avoid using the above comparisson
+
+// ===
+
+console.log("2" === 2);
