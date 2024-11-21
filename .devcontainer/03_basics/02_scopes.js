b24baf10b24c41c1b0327557d605cb009ff9e78c
princekhurana16 <77041387+princekhurana16@users.noreply.github.com>
2024-11-21 10:36:47 +0000

    Global and Local Scope

---
+++ .devcontainer/03_basics/02_scopes.js
@@ -0,0 +1,24 @@
+// let a = 10
+// const b = 20
+// var c = 30
+
+// var c = 300
+
+let a = 200
+if (true) {
+    let a = 10
+    const b = 20
+    var c = 30
+    console.log("INNER: ", a)
+    function addNum() {
+        
+    }
+}
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i]
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c);
