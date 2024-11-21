d61515572703e44cf46eac6cd8a06d4ec671b449
princekhurana16 <77041387+princekhurana16@users.noreply.github.com>
2024-11-21 10:54:09 +0000

    scope level and mini hoisting

--- .devcontainer/03_basics/02_scopes.js
+++ .devcontainer/03_basics/02_scopes.js
@@ -9,16 +9,62 @@ if (true) {
     let a = 10
     const b = 20
     var c = 30
-    console.log("INNER: ", a)
-    function addNum() {
-        
-    }
+    // console.log("INNER: ", a)
 }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i]
-}
+//for (let i = 0; i < array.length; i++) {
+//    const element = array[i]
+// }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+
+// Nested Scope
+
+function one() {
+    const username = "prince"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+    
+}
+
+// one()
+
+
+if (true) {
+    const username = "prince"
+    if (username  === "prince"){
+        const website = " youtube"
+       // console.log(username + website);   
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+
+
+
+// +++++++++++++++++++++++ interesting ++++++++++++++++++++++++++++++++++
+
+console.log(addone(5)) // will execute without any error 
+function addone(num){
+    return num + 1
+}
+
+
+addTwo(5) //will throw error 
+const addTwo = function(num) {
+    return num + 2
+}
+
