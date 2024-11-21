a7bce0cfba0067e2ec59d08d599eb6d0c4d0affc
princekhurana16 <77041387+princekhurana16@users.noreply.github.com>
2024-11-21 10:20:30 +0000

    functions with objects and arays

--- .devcontainer/03_basics/01_functions.js
+++ .devcontainer/03_basics/01_functions.js
@@ -44,4 +44,38 @@ function logInUserMessage(username = "sam"){
 
 // console.log(logInUserMessage("Prince"))
 
-console.log(logInUserMessage("hitesh"))
+// console.log(logInUserMessage("hitesh"))
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(2, 200, 400, 500, 2000))
+
+const user = {
+    username: "prince",
+    price: '199'
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`); 
+}
+
+// handleObject(user)
+
+handleObject({
+    username: "sam",
+    price: "399"
+})
+
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([200, 400, 100, 600]));
