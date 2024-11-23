37a7dfd4f40a087a05743d114ea19b736f6453c2
princekhurana16 <77041387+princekhurana16@users.noreply.github.com>
2024-11-23 09:23:00 +0000

    switch

---
+++ .devcontainer/04_control_flow/switch.js
@@ -0,0 +1,20 @@
+const month = "March"
+
+switch (month) {
+    case "January":
+        console.log("January");
+        break;
+    case "February":
+        console.log("February");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default case matched");
+        break;
+}
