2ca4331c4a235c935975b387581c2b5586f88c9b
princekhurana16 <77041387+princekhurana16@users.noreply.github.com>
2024-12-25 08:49:05 +0000

    inheritance in js

---
+++ 10_classes_and_oop/inheritance.js
@@ -0,0 +1,38 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
+    }
+}
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username)
+        this.email = email
+        this.password = password
+    }
+
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+    }
+}
+
+const chai = new Teacher("chai", "chai@gmail.com", "123")
+
+console.log(chai.addCourse());
+
+const masalachai = new User("masalachai")
+
+masalachai.logMe()
+
+console.log(chai === masalachai);
+
+console.log(chai === Teacher);
+
+console.log(chai instanceof Teacher);
+
+console.log(Teacher instanceof User);
+
