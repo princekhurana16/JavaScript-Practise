279b54aee1ead62a3575009196530e1c8ee4f19e
princekhurana16 <77041387+princekhurana16@users.noreply.github.com>
2024-12-31 06:40:25 +0000

    advance objects Property descriptor

---
+++ 10_classes_and_oop/Mathpi.js
@@ -0,0 +1,34 @@
+const descriptor = Object.getOwnPropertyDescriptor(Math, "PI")
+
+console.log(descriptor);
+
+const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isAvailable: true,
+
+    orderchai: function(){
+        console.log("chai nhi bani");
+        
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+
+Object.defineProperty(chai, 'name', {
+    writable: false,
+    enumerable: false
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+
+for (const [key, value] of Object.entries(chai)) {
+    if (typeof value !== 'function') {
+        console.log(`${key} : ${value}`);
+    }
+    
+}
+
+
